8fc311c85cf07e6d1e20b0b2372a68752b068151
Jimmy Carter <jcarter@marchex.com>
2016-03-25 08:54:18 -0700

    aggressively killing the chef-client

--- lib/shamwow/ssh/chef_stop.rb
+++ lib/shamwow/ssh/chef_stop.rb
@@ -2,7 +2,7 @@ module Shamwow; module SshTask;
 class Chef_stop
 
   def self.command
-    'sudo /etc/init.d/chef-client stop'
+    'sudo pkill -9 chef-client && sudo rm -f /var/run/chef/client.pid; ps -ef | grep chef-client'
   end
 
   def self.parse(host, data)
