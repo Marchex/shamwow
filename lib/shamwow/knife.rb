f0343cd6b2e65390999bf4a1968f0797238a8f43
Jimmy Carter <jcarter@marchex.com>
2017-03-22 13:01:12 -0700

    fixed run_list bug

--- lib/shamwow/knife.rb
+++ lib/shamwow/knife.rb
@@ -159,7 +159,7 @@ module Shamwow
       old_set.each do |id, v|
         if new_set[id].nil?
           rl = @runlists.first(:id => v)
-          @db.save_log('knife_runlist', host, 'delete', "Deleting link to runlist #{rl.name}-#{rl.version}")
+          @db.save_log('knife_runlist', host, 'delete', "Deleting link to runlist #{rl.name}")
           node.knife_runlist_links.first({ :runlist_id => id }).destroy
         end
       end
